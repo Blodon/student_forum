7f55f2e0618ba5e7f2b68263af73e38af1ad803d
Damian <student@localhost.localdomain>
2017-10-23 18:51:14 +0200

    czesc2

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resources :root
   resources :posts
   resources :topics
   resources :courses
   resources :students
+
+  root to: 'static#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
