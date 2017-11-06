3939eabc397f5a3a9653214a61a8e2a7ef38d52f
Your Name <you@example.com>
2017-11-06 18:51:12 +0100

    lab3

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -1,2 +1,5 @@
 module StudentsHelper
+  def current_student
+  Student.find_by(id: session[:student_id])
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :root
   resources :posts
   resources :topics
@@ -6,5 +8,10 @@ Rails.application.routes.draw do
   resources :students
 
   root to: 'static#index'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
